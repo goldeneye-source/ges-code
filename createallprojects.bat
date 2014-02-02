df229e665aac9e2e7ee6f20ca06aff10e4fc780b
Jonathan White <jon.white@dmapps.us>
2014-02-02 15:42:05 -0500

    Updated build scripts to incorporate a Boost::Python build and be more robust

--- createallprojects.bat
+++ createallprojects.bat
@@ -1,3 +1,3 @@
 pushd %~dp0
-devtools\bin\vpc.exe /hl2mp +everything /mksln everything.sln /2012
+devtools\bin\vpc.exe /ges +everything /mksln everything.sln /2012
 popd
--- creategameprojects.bat
+++ creategameprojects.bat
@@ -1,3 +1,3 @@
 pushd %~dp0
-devtools\bin\vpc.exe /hl2mp +game /mksln games.sln /2012
+devtools\bin\vpc.exe /ges +game /mksln games.sln /2012
 popd
