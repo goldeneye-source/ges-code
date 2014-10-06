a3026c8b43b9baffe707cf18a68a8be0112cdf29
neico <admin@neic0.de>
2014-10-06 06:23:19 +0200

    adjust git and project generation files

--- createallprojects.bat
+++ createallprojects.bat
@@ -1,1 +1,21 @@
-devtools\bin\vpc.exe /hl2mp +everything /mksln everything.sln
+@echo off
+
+if not "%VS120COMNTOOLS%" == "" (
+	set VisualStudio_Version=2013
+) else (
+	if not "%VS110COMNTOOLS%" == "" (
+		set VisualStudio_Version=2012
+	) else (
+		if not "%VS100COMNTOOLS%" == "" (
+			set VisualStudio_Version=2010
+		) else (
+			echo No Visual Studio C++ could be found, please install VS 2013/2012/2010 and try again
+			pause
+			exit
+		)
+	)
+)
+
+pushd %~dp0
+	devtools\bin\vpc.exe /hl2mp +everything /%VisualStudio_Version% /mksln everything-vs%VisualStudio_Version%.sln
+popd
--- creategameprojects.bat
+++ creategameprojects.bat
@@ -1,1 +1,21 @@
-devtools\bin\vpc.exe /hl2mp +game /mksln games.sln
+@echo off
+
+if not "%VS120COMNTOOLS%" == "" (
+	set VisualStudio_Version=2013
+) else (
+	if not "%VS110COMNTOOLS%" == "" (
+		set VisualStudio_Version=2012
+	) else (
+		if not "%VS100COMNTOOLS%" == "" (
+			set VisualStudio_Version=2010
+		) else (
+			echo No Visual Studio C++ could be found, please install VS 2013/2012/2010 and try again
+			pause
+			exit
+		)
+	)
+)
+
+pushd %~dp0
+	devtools\bin\vpc.exe /hl2mp +game /%VisualStudio_Version% /mksln game-vs%VisualStudio_Version%.sln
+popd
